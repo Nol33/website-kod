80b5d2d87327f56d15f44ad02ec30208d35912fa
nolwenlecunff <nolwenlecunff@gmail.com>
2020-01-11 23:18:07 +0100

    minor updates

--- src/Controller/DanemarkController.php
+++ src/Controller/DanemarkController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DanemarkController extends AbstractController
@@ -23,15 +24,52 @@ class DanemarkController extends AbstractController
 
     }
 
-/**
-* @Route("/danemark/city", name="danemark_city")
-*/
 
-    public function danemarkCity (/*PaysRepository $paysRepository*/)
+
+
+    /**
+     * @Route("/danemark/city", name="danemark_city")
+     */
+
+    public function danemarkCity(/*PaysRepository $paysRepository*/)
     {
 
         return $this->render('danemark_city.html.twig', [
 
+        ]);
+        // on retourne un fichier twig en lui envoyant la variable qui contient le danemark
+
+    }
+
+
+
+
+
+/**
+* @Route("/danemark/city/result", name="danemark_city_result")
+*/
+
+    public function danemarkCityResult (Request $request /*PaysRepository $paysRepository*/)
+    {
+
+      $result='';
+      $prix_jour=325;
+      $list_choix_jours=$request->request->get('list_choix_jours');
+      $list_choix_pers=$request->request->get('list_choix_pers');
+
+if ($list_choix_jours == 3 && $list_choix_pers == 1){
+    $result.=$list_choix_jours*$prix_jour;
+}
+
+if ($list_choix_jours == 3 && $list_choix_pers == 2){
+            $result.=$list_choix_jours*$prix_jour;
+        }
+
+
+
+
+        return $this->render('panier.html.twig', [
+'result' => $result,
         ]);
         // on retourne un fichier twig en lui envoyant la variable qui contient le danemark
     }
--- src/Controller/NorvegeController.php
+++ src/Controller/NorvegeController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class NorvegeController extends AbstractController
 {
@@ -22,4 +25,72 @@ class NorvegeController extends AbstractController
         ]);
         // on retourne un fichier twig en lui envoyant la variable qui contient le norvege
     }
+
+
+
+    /**
+     * @Route("/norvege/city", name="norvege_city")
+     */
+
+    public function norvegeCity(/*PaysRepository $paysRepository*/)
+    {
+
+        return $this->render('norvege_city.html.twig', [
+
+        ]);
+        // on retourne un fichier twig en lui envoyant la variable qui contient le danemark
+
+    }
+
+
+
+
+
+    /**
+     * @Route("/norvege/city/result", name="norvege_city_result")
+     */
+
+    public function norvegeCityResult (Request $request /*PaysRepository $paysRepository*/)
+    {
+
+        $result='';
+        $prix_jour=325;
+        $list_choix_jours=$request->request->get('list_choix_jours');
+        $list_choix_pers=$request->request->get('list_choix_pers');
+
+        if ($list_choix_jours == 3 && $list_choix_pers == 1){
+            $result.=$list_choix_jours*$prix_jour;
+        }
+
+        if ($list_choix_jours == 3 && $list_choix_pers == 2){
+            $result.=$list_choix_jours*$prix_jour;
+        }
+
+
+
+
+        return $this->render('panier.html.twig', [
+            'result' => $result,
+        ]);
+        // on retourne un fichier twig en lui envoyant la variable qui contient le danemark
+    }
+
+
+    /**
+     * @Route("/norvege/nature", name="norvege_nature")
+     */
+
+    public function norvegeNature (/*PaysRepository $paysRepository*/)
+    {
+
+        return $this->render('norvege_nature.html.twig', [
+
+        ]);
+        // on retourne un fichier twig en lui envoyant la variable qui contient le danemark
+    }
+
+
+
+
+
 }
